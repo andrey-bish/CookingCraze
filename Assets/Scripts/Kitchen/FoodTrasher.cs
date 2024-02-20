55395290cb14a84920af86c581b1a8cc46336219
Andrey Dubko <68705636+andrey-bish@users.noreply.github.com>
2024-02-20 22:12:29 +0300

    Implementation of the FoodTrasher.TryTrashFood method

--- Assets/Scripts/Kitchen/FoodTrasher.cs
+++ Assets/Scripts/Kitchen/FoodTrasher.cs
@@ -1,27 +1,61 @@
-using System;
-
 using UnityEngine;
 
 using JetBrains.Annotations;
+using UnityEngine.EventSystems;
 
 namespace CookingPrototype.Kitchen {
 	[RequireComponent(typeof(FoodPlace))]
-	public sealed class FoodTrasher : MonoBehaviour {
+	public sealed class FoodTrasher : MonoBehaviour , IPointerClickHandler{
 
-		FoodPlace _place = null;
-		float     _timer = 0f;
+		private FoodPlace _place = null;
+		
+		private int _clickedCount = 0;
+		
+		private float _clickTime = 0;
+		private float _clickDelay = 0.5f;
 
 		void Start() {
 			_place = GetComponent<FoodPlace>();
-			_timer = Time.realtimeSinceStartup;
+			_clickTime = Time.time;
 		}
 
 		/// <summary>
 		/// Освобождает место по двойному тапу если еда на этом месте сгоревшая.
+		/// Реализация через таймер
 		/// </summary>
 		[UsedImplicitly]
 		public void TryTrashFood() {
-			throw new NotImplementedException("TryTrashFood: this feature is not implemented");
+			
+			if ( _place.IsFree ) return;
+			
+			if ( Time.time - _clickTime > _clickDelay && _clickedCount > 0 ) ResetValues();
+
+			_clickedCount++;
+
+			if ( _clickedCount == 1 ) _clickTime =  Time.time;
+
+			if ( _clickedCount > 1 &&  Time.time - _clickTime < _clickDelay ) {
+				ResetValues();
+				if ( _place.CurFood.CurStatus == Food.FoodStatus.Overcooked ) _place.FreePlace();
+			}
+			else if ( _clickedCount > 2 ||  Time.time - _clickTime > _clickDelay ) _clickedCount = 0;
+		}
+
+		private void ResetValues() {
+			_clickedCount = 0;
+			_clickTime = 0;
+		}
+
+		/// <summary>
+		/// Освобождает место по двойному тапу если еда на этом месте сгоревшая.
+		/// Реализация через EventSystems
+		/// </summary>
+		public void OnPointerClick(PointerEventData eventData) {
+			if ( _place.IsFree ) return;
+			
+			if (eventData.clickCount == 2) {
+				if ( _place.CurFood.CurStatus == Food.FoodStatus.Overcooked ) _place.FreePlace();
+			}
 		}
 	}
 }
